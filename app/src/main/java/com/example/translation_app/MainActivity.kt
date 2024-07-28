322a492d4a97c3bc99c1cffe1bf76cbbb1caeacb
Abdul-Moiz-Khan1 <moizz1326@gmail.com>
2024-07-28 14:15:19 +0500

    under dev

--- app/src/main/java/com/example/translation_app/MainActivity.kt
+++ app/src/main/java/com/example/translation_app/MainActivity.kt
@@ -2,33 +2,47 @@ package com.example.translation_app
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.translation_app.databinding.ActivityMainBinding
 import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.nl.languageid.LanguageIdentification
 import com.google.mlkit.nl.languageid.LanguageIdentificationOptions
-import com.google.mlkit.nl.languageid.LanguageIdentifier
 import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.TranslatorOptions
-import java.util.ArrayList
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var Languages: MutableList<String>
-    private lateinit var language_codes:List<String>;
+    private lateinit var language_codes:List<String>
     lateinit var return_langauge:String
+    private lateinit var from_spinner:Spinner
+    private lateinit var to_spinner:Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
         return_langauge = "en"
+
+        Languages = ArrayList<String>()
+        language_codes = ArrayList<String>()
+
+        from_spinner = findViewById(R.id.fromM)
+        to_spinner= findViewById(R.id.to)
+
+        from_spinner.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item , Languages)
+        to_spinner.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item , Languages)
+
+//        from_spinner.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item , Languages)
+//        to_spinner.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item , Languages)
+
 
         set_language_list()
 
@@ -36,12 +50,13 @@ class MainActivity : AppCompatActivity() {
             val getsentence = findViewById<EditText>(R.id.sentence).text.toString()
             val detected_language = detect_Language(getsentence)
             Log.d("check lan" , detected_language)
+
             val options = TranslatorOptions.Builder()
-                .setSourceLanguage(TranslateLanguage.THAI)
+                .setSourceLanguage(TranslateLanguage.fromLanguageTag(detected_language).toString())
                 .setTargetLanguage(TranslateLanguage.ENGLISH)
                 .build()
-            language_codes = TranslateLanguage.getAllLanguages()
 
+            language_codes = TranslateLanguage.getAllLanguages()
             val englishGermanTranslator = Translation.getClient(options)
 
             var conditions = DownloadConditions.Builder()
